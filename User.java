e0d1b5d6a5f58d2c8615be105a449963cc334dc6
joanna <1981810999@qq.com>
2020-07-16 16:32:26 +0800

    java2.work

---
+++ User.java
@@ -0,0 +1,10 @@
+package TS;
+
+public class User {
+	@Override
+	public String toString() {
+		return "jdbcTemplate [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+}
